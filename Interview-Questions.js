00abe6d0394359863a87b677ef39ef36b51e3db7
Cole Rudolph <crudolph@gmail.com>
2019-06-20 21:32:40 -0400

    added files

---
+++ Interview-Questions.js
@@ -0,0 +1,22 @@
+function fizzbuzz() {
+    for (var i = 1 <= 100; i++) {
+        document.write(i)
+        if (i % 3 && i % 5) {
+            document.write(FizzBuzz);
+        }
+    } 
+}
+
+
+
+// for (var x=1; x <= 100; x++){
+//     if( x % 3 && x % 5 ) {
+//         document.write(x);
+//     } else {
+//         if( x % 3 == 0 ) {
+//             document.write("ping");
+//         }
+//         if( x % 5 == 0 ) {
+//             document.write("pong");
+//         }
+//     }
